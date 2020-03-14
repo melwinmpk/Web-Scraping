d3454dc6fba133a066c5826aed890c2af582137b
Melwinmpk <melwinmpk13@gmail.com>
2020-03-14 11:09:26 +0530

    single page getting links

---
+++ basic.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
+import re
+''' This Code is all about getting links in <a> tag getting href data storing in a csv file '''
+my_url = "https://www.toprankers.com"
+
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+page_soup = soup(page_html, "html.parser")
+links = page_soup.findAll("a")
+
+filename = "links.csv"
+f = open(filename,"w")
+headers="Links\n"
+f.write(headers)
+for link in page_soup.findAll("a", attrs={'href': re.compile("")}):
+    f.write(link.get('href')+"\n")
+    # print(link.get('href'))
+
+f.close()
+
